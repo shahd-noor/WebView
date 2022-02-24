46a4f610ed310d7020175d0f5c4fdcac0b01231e
Asus <Asus@DESKTOP-27A2UR1>
2022-02-25 00:31:38 +0300

    Add project files.

---
+++ WebView/WebView/WebViewPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WebView
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class WebViewPage : ContentPage
+    {
+        public WebViewPage()
+        {
+            InitializeComponent();
+        //    BrowserLink.source = "https://www.google.com/";
+        }
+        private void Add_Clicked(object sender ,EventArgs e)
+        {
+            if (string.IsNullOrEmpty(WebLink.Text))
+            {
+                DisplayAlert("Warning", "Please enter webLink", "Ok");
+                    return;
+
+            }
+            string.Url = "https://" + WebLink.Text;
+            BrowserLink.Source = Url;
+
+        }
+    }
+}
